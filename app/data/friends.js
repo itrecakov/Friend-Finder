09177b22618e3c26a7537169603763ae4eeab5b2
Iva Trecakov <itreckov@gmail.com>
2020-05-03 17:27:48 -0400

    completed assignment

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friendMatch = [
+    {
+        name: "Monica Geller",
+        photo: "https://vignette.wikia.nocookie.net/friends/images/2/2f/Monica_Geller-Bing_Season_10.png/revision/latest?cb=20180426182234",
+        scores: ["1", "1", "1", "1", "1"]
+    }, {
+        name: "Rachel Green",
+        photo: "https://im.rediff.com/movies/2013/may/13hollywood1.jpg",
+        scores: ["2", "2", "2", "2", "2"]
+    }, {
+        name: "Ross Geller",
+        photo: "https://static3.srcdn.com/wordpress/wp-content/uploads/2020/03/ross-geller-Cropped.jpg",
+        scores: ["3", "3", "3", "3", "3"]
+    }, {
+        name: "Chandler Bing",
+        photo: "https://vignette.wikia.nocookie.net/friends/images/9/99/Season_2_chandler.png/revision/latest/scale-to-width-down/340?cb=20200326151017",
+        scores: ["4", "4", "4", "4", "4"]
+    },
+
+];
+
+
+
+module.exports = friendMatch;
---
+++ server.js
@@ -0,0 +1,19 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 8080;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
